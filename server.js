907f565ffd8aeb453ffcd2727c7a56808e1d41c7
Juan Trinidad <juan_trinidad14@hotmail.com>
2020-12-12 16:38:13 -0500

    add redux products and fixed server

--- server.js
+++ server.js
@@ -1,46 +1,44 @@
-const express = require("express")
-const bodyParser = require("body-Parser")
-const mongoose = require("mongoose")
-const shortid = require("shortid")
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const shortid = require("shortid");
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost/shopping-cart-db",{
-    useNewUrlParser: true,
-    useCreateindex: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect("mongodb://localhost/react-shopping-cart-db", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 const Product = mongoose.model(
-    "products",
-    new mongoose.Schema({
-        _id : {type: String, default: shortid.generate},
-        title: String, 
-        description: String, 
-        image: String, 
-        price: String, 
-        availableSizes: [String],
-        
-    }) 
-)
+  "products",
+  new mongoose.Schema({
+    _id: { type: String, default: shortid.generate },
+    title: String,
+    description: String,
+    image: String,
+    price: Number,
+    availableSizes: [String],
+  })
+);
 
-app.get("/api/products", async (req,res) =>{
+app.get("/api/products", async (req, res) => {
+  const products = await Product.find({});
+  res.send(products);
+});
 
-    const products = await Product.find({})
-    res.send(products)
-})
+app.post("/api/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  const savedProduct = await newProduct.save();
+  res.send(savedProduct);
+});
 
-app.post("/api/products", async (req,res) =>{
-    const newProduct = new Product(req.body)
-    const savedProduct = await newProduct.save()
-    res.send(savedProduct)
-})
+app.delete("/api/products/:id", async (req, res) => {
+  const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+  res.send(deletedProduct);
+});
 
-app.delete("/api/products/:id", async(req, res) => {
-    const  deletedProduct = await Product.findByIdAndDelete(req.params.id)
-    res.send(deletedProduct)
-})
-
-const port = process.env.PORT || 5000
-app.listen(port, () => console.log("server at http://localhost:5000"))
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("server at http://localhost:5000"));
