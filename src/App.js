fa2fb10295f120005953a16b26742083e84103dd
Juan Trinidad <juan_trinidad14@hotmail.com>
2020-12-12 20:14:38 -0500

    fixed server

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class App extends React.Component {
       <Provider store={store}>
         <div className="grid-container">
           <header>
-            <a href="/">React Shopping Cart</a>
+            <a href="/">Shopping Cart</a>
           </header>
           <main>
             <div className="content">
